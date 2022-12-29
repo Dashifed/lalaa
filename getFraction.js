fcdcc76482acfe108f7a2b77f68752e3e2a5e700
Dashifed <cubeydash@outlook.com>
2022-12-29 08:29:03 +0100

    n.1

---
+++ getFraction.js
@@ -0,0 +1,59 @@
+function getFraction() {
+  let text = prompt("Encrypt text:");
+  if (text === null) {
+    return;
+  }
+  let splitText = text.split("");
+
+  const dict = {
+    " ": "000",
+    a: "001",
+    b: "002",
+    c: "003",
+    d: "004",
+    e: "005",
+    f: "006",
+    g: "007",
+    h: "008",
+    i: "009",
+    j: "010",
+    k: "011",
+    l: "012",
+    m: "013",
+    n: "014",
+    o: "015",
+    p: "016",
+    q: "017",
+    r: "018",
+    s: "019",
+    t: "020",
+    u: "021",
+    v: "022",
+    w: "023",
+    x: "024",
+    y: "025",
+    z: "026",
+  };
+
+  let mappedText = splitText.map((key) => dict[key]);
+  let decimal = "0." + mappedText.join("");
+
+  //Turn decimal to fraction
+  let numerator = decimal;
+  let denominator = 1;
+  while (numerator % 1 !== 0) {
+    numerator *= 10;
+    denominator *= 10;
+  }
+  let gcd = function (a, b) {
+    if (b === 0) {
+      return a;
+    } else {
+      return gcd(b, a % b);
+    }
+  };
+  let divisor = gcd(numerator, denominator);
+  numerator /= divisor;
+  denominator /= divisor;
+  alert(numerator + "/" + denominator);
+}
---
+++ getText.js
@@ -0,0 +1,55 @@
+function getText() {
+  let fraction = prompt("Insert that fraction over here:");
+  if (fraction === null || isNaN(fraction)) {
+    return;
+  }
+  let fractionArray = fraction.split("/");
+  let numerator = fractionArray[0];
+  let denominator = fractionArray[1];
+  let decimal = numerator / denominator;
+  let splitDecimal = decimal.toString().split(".");
+  let num = splitDecimal[1];
+  let output = [];
+  let current = "";
+  for (let i = 0; i < num.length; i++) {
+    if (i % 3 == 0 && i != 0) {
+      output.push(current);
+      current = "";
+    }
+    current += num[i];
+  }
+  output.push(current);
+
+  const dict = {
+    "000": " ",
+    "001": "a",
+    "002": "b",
+    "003": "c",
+    "004": "d",
+    "005": "e",
+    "006": "f",
+    "007": "g",
+    "008": "h",
+    "009": "i",
+    "010": "j",
+    "011": "k",
+    "012": "l",
+    "013": "m",
+    "014": "n",
+    "015": "o",
+    "016": "p",
+    "017": "q",
+    "018": "r",
+    "019": "s",
+    "020": "t",
+    "021": "u",
+    "022": "v",
+    "023": "w",
+    "024": "x",
+    "025": "y",
+    "026": "z",
+  };
+
+  let map = output.map((key) => dict[key]);
+  console.log(map.join(""));
+}
